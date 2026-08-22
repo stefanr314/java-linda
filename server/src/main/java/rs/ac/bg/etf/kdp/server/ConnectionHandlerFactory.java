17a9179294e7d0f3dce873eadb09fa66015e3a3a
stefanr <stefanristic01@yahoo.com>
2026-08-22 22:54:07 +0200

    Implement finding free stations mechanism. Commit:   - Implement methods for acquiring and releasing the available slot     handle upon workstation context. This value must be updated     atomically in order to provide concurrent use.   - Implement method for reaching the free station on workstation     registry. This method is thread safe (as guaranteed by concurrent     hash map implementation) but might see stale value of (in meantime)     disconnected stations. It is the responsibility of therad that send     message to the workstation to handle this state.   - Provide tests for testing both thread safety and correct logical     implementation.   - Change Socket reference type to Closeable since previous type was     giving away way too much with no use (on WorkstationContext).

--- server/src/main/java/rs/ac/bg/etf/kdp/server/ConnectionHandlerFactory.java
+++ server/src/main/java/rs/ac/bg/etf/kdp/server/ConnectionHandlerFactory.java
@@ -12,6 +12,14 @@ import java.net.Socket;
 /**
  * Simple Factory for generation of connection handlers. This pattern does not resemble Factory Pattern but rather a
  * good programming idiom for delegating the creational process to encapsulated instance.
+ * <p>
+ * This class will be approached by multiple threads; but this doesn't yield synchronization use since the class
+ * is effectively immutable so the only requirement is to properly publish this object. Since the instance fields
+ * are declared as final JMM guarantees the proper publication of the instance and it's fields. All the methods
+ * that live on this object are not mutating any of the invariants so their use is thread safe.
+ * </p>
+ *
+ * @author stefanr
  */
 public class ConnectionHandlerFactory {
 	private final WorkstationRegistry workstationRegistry;
@@ -22,6 +30,16 @@ public class ConnectionHandlerFactory {
 		this.jobRegistry = jobRegistry;
 	}
 
+	/**
+	 * Method for reaching the proper handler read from introductory message. This method is synchronization free
+	 * since it enforces the stack confinement approach.
+	 *
+	 * @param hello  introductory message
+	 * @param socket socket connection to sender
+	 * @param out    stream for writing objects
+	 * @param in     stream for reading objects
+	 * @return handler for the rest of communication
+	 */
 	public ConnectionHandler getHandler(Hello hello, Socket socket, ObjectOutputStream out, ObjectInput in) {
 		ConnectionHandler handler = null;
 
--- server/src/main/java/rs/ac/bg/etf/kdp/server/WorkstationRegistry.java
+++ server/src/main/java/rs/ac/bg/etf/kdp/server/WorkstationRegistry.java
@@ -14,12 +14,50 @@ public final class WorkstationRegistry {
 
 	private final Map<String, WorkstationContext> workstations = new ConcurrentHashMap<>();
 
+	/**
+	 * Registers a workstation as available for scheduling.
+	 *
+	 * @param context required info about the workstation.
+	 */
 	public void register(WorkstationContext context) {
 		workstations.put(context.workstationInfo().hostName(), context);
 	}
 
+	/**
+	 * Removes a workstation from consideration, e.g. after a missed
+	 * heartbeat.
+	 * <p>
+	 * It is required that remove is performed with the context in mind, since otherwise a slow handler response will
+	 * remove fresh connection (e.g. upon workstation losing connection and then returning back).
+	 * </p>
+	 *
+	 * @param context required info about the workstation.
+	 */
+
 	public boolean unregister(WorkstationContext context) {
 		return workstations.remove(context.workstationInfo().hostName(), context);
+	}
+
+	/**
+	 * Method for trying to gain free stations (i.e. slot of station). Method does not enforce blocking of any
+	 * nature, just returns false if there are not any free stations (in the moment of checking). Otherwise, a
+	 * context of free station is returned.
+	 * <p>
+	 * Iteration on concurrent hashmap if thread safe since no exception will be thrown, and even the concurrent
+	 * modifications might be present in the moment of iterating (by specification this is not guaranteed).
+	 * However, iterating on this map does not guarantee that no stale values will be present, which in turn
+	 * returns the slot for unreachable i.e. stale station. This is expected behaviour. Responsibility is
+	 * delegated to the thread that sends the request to station ( handler of client requests per se).
+	 * </p>
+	 *
+	 * @return context of available workstation, or empty value holder.
+	 */
+	public Optional<WorkstationContext> tryFindFreeStation() {
+		for (WorkstationContext workstation : workstations.values()) {
+			if (workstation.tryAcquireSlot())
+				return Optional.of(workstation);
+		}
+		return Optional.empty(); //todo
 	}
 
 	public Collection<WorkstationContext> workstations() {
